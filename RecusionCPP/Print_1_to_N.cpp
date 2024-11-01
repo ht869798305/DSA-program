222ed810e3a1a14d965ea5aeea44bdc3201e1c29
Harsh Thakur <ht8697983@gmail.com>
2024-11-01 23:29:45 +0530

    done with printing 1 to N problem and ignored all exe file by adding *.exe to .gitignore file

---
+++ RecusionCPP/Print_1_to_N.cpp
@@ -0,0 +1,26 @@
+// C++ program to How will you print numbers from 1 to n without using a loop?
+#include <iostream>
+using namespace std;
+
+class Print {
+
+    // It prints numbers from 1 to n.
+public:
+    void printNos(unsigned int n)
+    {
+        if (n > 0) {
+            printNos(n - 1);
+            cout << n << " ";
+        }
+        return;
+    }
+};
+
+// Driver code
+int main()
+{
+    int n = 10;  // it should print from 1 to 10
+    Print p;
+    p.printNos(n);
+    return 0;
+}
